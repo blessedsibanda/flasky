d50c3fc29b27b60af9384d513b8d1ec641fa0490
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-16 11:55:15 +0200

    generate user confirmation token

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+from app import db  
 from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
@@ -20,3 +22,28 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User()
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+        self.assertTrue(u.confirmed)
+
+    def test_invalid_confirmation_token(self):
+        u1 = User()
+        u2 = User()
+        db.session.add_all([u1, u2])
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
+        self.assertFalse(u2.confirmed)
+
+    def test_expired_confirmation_token(self):
+        u = User()
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(expiration=1)
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
