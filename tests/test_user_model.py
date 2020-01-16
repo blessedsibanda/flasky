6038b472c5a601d5196b09fe51df3c4d125bc89f
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-16 15:00:13 +0200

    more user_model tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -47,3 +47,45 @@ class UserModelTestCase(unittest.TestCase):
         token = u.generate_confirmation_token(expiration=1)
         time.sleep(2)
         self.assertFalse(u.confirm(token))
+
+    def test_valid_reset_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_reset_token()
+        self.assertTrue(User.reset_password(token, 'dog'))
+        self.assertTrue(u.verify_password('dog'))
+
+    def test_invalid_reset_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_reset_token()
+        self.assertFalse(User.reset_password(token + 'a', 'dog'))
+        self.assertTrue(u.verify_password('cat'))
+
+    def test_email_change_token(self):
+        u = User(email='foo@example.com')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_email_change_token('bar@example.com')
+        self.assertTrue(u.change_email(token))
+        self.assertEqual(u.email, 'bar@example.com')
+
+    def test_email_change_token(self):
+        u = User(email='foo@example.com')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_email_change_token('bar@example.com')
+        self.assertFalse(u.change_email(token + 'a'))
+        self.assertNotEqual(u.email, 'bar@example.com')
+        self.assertEqual(u.email, 'foo@example.com')
+
+    def test_duplicate_emai_change_token(self):
+        u1 = User(email='john@example.com')
+        u2 = User(email='peter@example.com')
+        db.session.add_all([u1, u2])
+        db.session.commit()
+        token = u1.generate_email_change_token(u2.email)
+        self.assertFalse(u1.change_email(token))
+        self.assertEqual(u1.email, 'john@example.com')
