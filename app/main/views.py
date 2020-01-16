2bf672abc934dfef3d8159d6e2017852623b2a60
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-16 16:15:22 +0200

    test user roles

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,9 @@ from flask import render_template, session, redirect, url_for, current_app
 from flask_login import login_required
 from . import main
 from .. import db    
-from ..models import User
+from ..models import User, Permission
 from ..email import send_email
+from ..decorators import admin_required, permission_required
 
 @main.route('/')
 def index():
@@ -14,3 +15,15 @@ def index():
 @login_required
 def user():
     return render_template('main/user.html')
+
+@main.route('/admin')
+@login_required
+@admin_required
+def for_admins_only():
+    return "For Administrators!"
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE)
+def for_moderators_only():
+    return "For comment moderators!"
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,11 +1,12 @@
 import unittest
 import time
 from app import db  
-from app.models import User
+from app.models import User, Permission, AnonymousUser, Role
 
 class UserModelTestCase(unittest.TestCase):
     def setUp(self):
         db.create_all()
+        Role.insert_roles()
 
     def tearDown(self):
         db.session.remove()
@@ -96,3 +97,19 @@ class UserModelTestCase(unittest.TestCase):
         token = u1.generate_email_change_token('peter@example.com')
         self.assertFalse(u1.change_email(token))
         self.assertEqual(u1.email, 'john@example.com')
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
