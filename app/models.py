d18d83b3a956de3f0af6ad9c3ac37404aa8e47a3
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-17 08:06:37 +0200

    display blog posts on home page

--- app/models.py
+++ app/models.py
@@ -92,6 +92,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def gravatar(self, size=160, default='identicon', rating='g'):
         url = 'https://secure.gravatar.com/avatar'
@@ -193,3 +194,11 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
