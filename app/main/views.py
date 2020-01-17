616ebecbfb1066060e997f19af13ca7860af328a
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-01-17 20:06:27 +0200

    display user's posts on their profile page

--- app/main/views.py
+++ app/main/views.py
@@ -22,7 +22,8 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('main/user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('main/user.html', user=user, posts=posts)
 
 @main.route('/admin')
 @login_required
